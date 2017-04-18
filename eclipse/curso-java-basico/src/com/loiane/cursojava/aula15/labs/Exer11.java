d16faced33d3ecf4cfa9787544e37845f3f0d669
Kleber MArtins Alves <kleber_kma@hotmail.com>
2017-04-17 22:11:12 -0300

    Exercícios feitos no Final de semana e hoje.

---
+++ eclipse/curso-java-basico/src/com/loiane/cursojava/aula15/labs/Exer11.java
@@ -0,0 +1,36 @@
+package com.loiane.cursojava.aula15.labs;
+
+import java.util.Scanner;
+
+public class Exer11 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Digite o salário:");
+        double salario = scan.nextDouble();
+        int percentual = 0;
+        
+        if(salario <= 280){
+        	percentual = 20;
+        }else if(salario > 280 && salario <= 700){
+        	percentual = 15;       	
+        }else if(salario > 700 && salario <= 1500){
+        	percentual = 10;
+        }else if(salario > 1500){
+        	percentual = 5;
+        }
+        
+        double valorAumento = (salario / 100) * percentual;
+        double salarioAjutado = salario + valorAumento;
+        
+        System.out.println("Relatório de Ajuste Salirial");
+        System.out.println("Salario anterior: " + salario);
+        System.out.println("Percentual de aumento aplicado: " + percentual);
+        System.out.println("Valor do aumento: " + valorAumento);
+        System.out.println("Salario ajustado: " + salarioAjutado);
+
+	}
+
+}
