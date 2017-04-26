412d51c166afd9790192ef67f5d0f97271297bfe
VMK_NOTBOOK\klebe <kleber_kma@hotmail.com>
2017-04-25 22:15:55 -0300

    Correção

--- eclipse/curso-java-basico/src/com/loiane/cursojava/aula15/labs/Exer06.java
+++ eclipse/curso-java-basico/src/com/loiane/cursojava/aula15/labs/Exer06.java
@@ -13,9 +13,9 @@ public class Exer06 {
 		int num2 = scan.nextInt();
 		int num3 = scan.nextInt();
 		
-		if(num1 > num2 && num1 > num3){
+		if(num1 >= num2 && num1 >= num3){
 			System.out.println(num1 + " é o maior Número");
-		} else if(num2 > num1 && num2 > num3){
+		} else if(num2 >= num1 && num2 >= num3){
 			System.out.println(num2 + " é o maior Número");
 		}else {
 			System.out.println(num3 + " é o maior Número");
